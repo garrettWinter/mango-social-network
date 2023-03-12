7ddd22f54cb021869d845857b4f129a8da4740a9
Garrett Winter <Garrett_Winter@hotmail.com>
2023-03-12 00:06:15 -0600

    commented out the thought referances so the server would load. Also removed a bug from the email validation that was causing the server to fail load.

--- models/users.js
+++ models/users.js
@@ -4,13 +4,14 @@ const userSchema = new Schema(
     {
         username: { type: String, required: true, trim: true, unique: true },
         email: {
-            type: String, required: true, unique: true, validate: {
-                function(email) {
-                    return /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/.test(email); // Removed the carrot and the dollarsign could try to readd later, or not
+            type: String, required: true, unique: true,
+            validate: {
+                validator: function(email) {
+                    return /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/.test(email);
                 },
                 message: emailCheck => `${emailCheck.value} is not a valid email address!`
             }
-        }, //Needs match a valid email address (look into Mongoose's matching validation)
+        },
         thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought' }], // Array of `_id` values referencing the `Thought` model
         friends: [{ type: Schema.Types.ObjectId, ref: 'user' }], // Array of `_id` values referencing the `User` model (self-reference)
     },
